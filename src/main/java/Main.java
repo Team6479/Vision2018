da1cb9a9eeb4bb634ce2a066a7ba2d3be6f7d634
jacob-abraham <jacob.r.abraham@gmail.com>
2018-01-17 22:48:46 -0700

    worked on making it work on jetson, need to nativly build libraries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,25 @@
 
 import org.opencv.core.Mat;
 
-import edu.wpi.cscore.UsbCamera;
+//import edu.wpi.cscore.UsbCamera;
 
 public class Main {
 	public static void main(String[] args) {
 		// Loads our OpenCV library. This MUST be included
-		System.loadLibrary("opencv_java310");
+		System.out.println("PATH: " + System.getProperty("java.library.path"));
+		try {
+		//System.loadLibrary("libopencv_core");
+		}
+		catch (UnsatisfiedLinkError e) {
+			e.printStackTrace();
+		}
+		System.load("/usr/lib/libopencv_core.so");
+		try {
+		//System.load("/usr/lib/libopencv_core.so");
+		}
+		catch (UnsatisfiedLinkError e) {
+			e.printStackTrace();
+		}
 
 		Mat mat = new Mat();
 		//UsbCamera camera = new UsbCamera("Camera", 0);
